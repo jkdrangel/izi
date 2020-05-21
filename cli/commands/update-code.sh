2ebaa0eff8b8c9a611ce56e2c18cbceca7085cdb
Lucas Santana Carneiro <jkdrangel@hotmail.com>
2020-05-21 11:23:26 -0300

    Update update-code.sh

--- cli/commands/update-code.sh
+++ cli/commands/update-code.sh
@@ -1,5 +1,5 @@
 izi get wget
 filename=$IZI_CLI/vscode.deb
-wget -o $filename https://go.microsoft.com/fwlink/?LinkID=760868
+wget -o $filename https://go.microsoft.com/fwlink/\?LinkID\=760868
 sudo dpkg -i $filename
 rm $filename
